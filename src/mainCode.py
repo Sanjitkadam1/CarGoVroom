6feb597b7d08ed866208fad9e85d6a03b3bf73cc
SCkadam <bimbisara3@gmail.com>
2024-07-09 21:00:45 -0500

    Init code added to main code

--- src/mainCode.py
+++ src/mainCode.py
@@ -1,10 +1,39 @@
-import time 
-import RPi.GPIO
+#-------------------------Packages--------------------------#
+
+import time as t
+import os
+os.system ("sudo pigpiod")
+print("importing packages...")
+t.sleep(1)
+import pigpio
+import RPi.GPIO # type: ignore
 import numpy as np
+import cv2 as cv
+from picamera2 import Picamera2 # type: ignore
 
+#-------------------------Init Code-------------------------#
+#	General init
 ending = False
+START = t.t()
 
-START = time.time()
+#	Motor init
+pi = pigpio.pi()
+esc = 14
+pi.set_servo_pulsewidth(esc, 0) 
+max_motor = 1500 #max motor speed
+min_motor = 500
+
+#	Servo init
+servo = 15
+min_servo = 550  # 1 ms
+max_servo = 2450  # 2 ms
+mid_servo = 1550  # 1.55 ms
+
+def set_servo_position(pulse_width):
+    pi.set_servo_pulsewidth(servo, pulse_width)
+
+
+
 
 
 
@@ -12,13 +41,13 @@ while not ending:
 	
     # Implement for frame 
 	
-	X, color = detectColor(img)
+	X, color = detectColor(img) # type: ignore
 	if X != None:
 		if (color == "red") and X :
 			
 
 
-def detectColor(img):
+def detectColor(img): # type: ignore
 	# ASSIGN THRESHOLD
 	thresh = 20
 	hsvimg = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -36,9 +65,9 @@ def detectColor(img):
 	lower_red2 = np.array([170, 100, 100])
 	upper_red2 = np.array([180, 255, 255])
 
-	redmask1 = cv2.inRange(img, lower_red1, upper_red1)
-	redmask2 = cv2.inRange(img, lower_red2, upper_red2)
-	redMask = cv2.bitwise_or(redmask1, redmask2)
+	redmask1 = cv.inRange(img, lower_red1, upper_red1)
+	redmask2 = cv.inRange(img, lower_red2, upper_red2)
+	redMask = cv.bitwise_or(redmask1, redmask2)
 	redper = np.count_nonzero(redMask)
 
 	if (redper > thresh):
@@ -58,11 +87,11 @@ def detectColor(img):
 					elif x >= greatestX and y >= greatestY and redMask[x-5, y-5] == 255:
 						greatestX = x
 						greatestY = y
-						
-        midX = (greatestX + smallestX)/2
-        return midX, "red"
-
-    if (greenper > thresh):
+		
+		midX = (greatestX + smallestX)/2
+		return midX, "red"
+	
+	if (greenper > thresh):
 		smallestX, smallestY = 100000000, 100000000
 		greatestX, greatestY = -1, -1
 		
@@ -79,17 +108,16 @@ def detectColor(img):
 					elif x >= greatestX and y >= greatestY and redMask[x-5, y-5] == 255:
 						greatestX = x
 						greatestY = y
-        midX = (greatestX + smallestX)/2
-        return midX, "green"
-
-    return None
+		
+		midX = (greatestX + smallestX)/2
+		return midX, "green"
+	
+	return None
 
 def turn():
 	# Need to implement
 	return NotImplementedError
 
-def setSpeed():
-	 
+def setSpeed(): # type: ignore
 
-def motorInit():
-	
+	print()
