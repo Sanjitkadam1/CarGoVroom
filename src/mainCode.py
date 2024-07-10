a979624e604c2059904553559bdee9e74fa70262
in-sol-ence <sol.oberoi.m@gmail.com>
2024-07-09 22:58:29 -0500

    Added init code

--- src/mainCode.py
+++ src/mainCode.py
@@ -6,15 +6,14 @@ os.system ("sudo pigpiod")
 print("importing packages...")
 t.sleep(1)
 import pigpio
-import RPi.GPIO # type: ignore
+import RPi.GPIO as PIN # type: ignore
 import numpy as np
 import cv2 as cv
 from picamera2 import Picamera2 # type: ignore
 
 #-------------------------Init Code-------------------------#
 #	General init
-ending = False
-START = t.t()
+START = t.time()
 
 #	Motor init
 pi = pigpio.pi()
@@ -24,27 +23,40 @@ max_motor = 1500 #max motor speed
 min_motor = 500
 
 #	Servo init
-servo = 15
+servo = 0 # set servo pin
 min_servo = 550  # 1 ms
 max_servo = 2450  # 2 ms
 mid_servo = 1550  # 1.55 ms
 
-def set_servo_position(pulse_width):
-    pi.set_servo_pulsewidth(servo, pulse_width)
+#   Mini Servo init 
+# raise NotImplementedError
 
+#   Depth init
+TRIG1 = 0  # Front
+TRIG2 = 0  # Left
+TRIG3 = 0  # Right
+ECHO1 = 0
+ECHO2 = 0
+ECHO3 = 0
+# set values in the depth function
+# raise NotImplementedError
+PIN.setmode(PIN.BCM)
+PIN.setup(TRIG1, PIN.OUT)
+PIN.output(TRIG1, PIN.LOW)
+PIN.setup(ECHO1, PIN.IN)
+PIN.setup(TRIG2, PIN.OUT)
+PIN.output(TRIG2, PIN.LOW)
+PIN.setup(ECHO2, PIN.IN)
+PIN.setup(TRIG3, PIN.OUT)
+PIN.output(TRIG3, PIN.LOW)
+PIN.setup(ECHO3, PIN.IN)
 
+# Camera init
+#-------------------------Main Code--------------------------#
+# Record start values
+LeftYstart = depth(1, 0)
+RightYstart = depth(2, 0)
 
-
-
-
-while not ending:
-	
-    # Implement for frame 
-	
-	X, color = detectColor(img) # type: ignore
-	if X != None:
-		if (color == "red") and X :
-			
 
 
 def detectColor(img): # type: ignore
@@ -114,10 +126,47 @@ def detectColor(img): # type: ignore
 	
 	return None
 
-def turn():
+def turn90():
 	# Need to implement
 	return NotImplementedError
 
 def setSpeed(): # type: ignore
 
 	print()
+
+def bigServo(pulse_width):
+	servo = 0 
+	# Set this to the GPIO pin
+	raise NotImplementedError
+    pi.set_servo_pulsewidth(servo, pulse_width)
+
+def depth(num, speed):
+	if num == 0: #Front 
+		TRIG = 0
+		ECHO = 0
+	if num == 1: #Left 
+		TRIG = 0
+		ECHO = 0
+	if num == 2: #Right
+		TRIG = 0
+		ECHO = 0
+
+	PIN.output(2, PIN.HIGH)
+    t.sleep(0.00001)   # Creating a 10uS (microsecond) pulse
+	PIN.output(2, PIN.LOW)
+    
+	while PIN.input(3)==0:
+    	pulse_start = t.time()
+
+    while PIN.input(3)==1:
+    	pulse_end = t.time()
+    
+    rawDist = pulse_end - pulse_start
+
+	#Implement speed divider
+
+    cmDist = rawDist * 34600/2 
+    #34600 cm/s is the speed of sound in room temprature air. speed * time = distance. divided by 2 bcz its a round trip
+    
+    cmDist = round(cmDist, 2)
+    return cmDist
