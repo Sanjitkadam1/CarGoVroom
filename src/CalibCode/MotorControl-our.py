ab1b0230ee103d184a4b7d4fb781b4e06f339441
in-sol-ence <sol.oberoi.m@gmail.com>
2024-09-14 16:46:21 -0500

    Servo control stuff

--- src/CalibCode/MotorControl-our.py
+++ src/CalibCode/MotorControl-our.py
@@ -10,18 +10,19 @@ pi.set_servo_pulsewidth(ESC, 0)
 
 max_value = 1500 
 min_value = 500
-
-pi.set_servo_pulsewidth(ESC, 0)
-print("Calibration start")
-pi.set_servo_pulsewidth(ESC, max_value)
-pi.set_servo_pulsewidth(ESC, min_value)
-time.sleep(7)
-print ("Calibrating....")
-time.sleep (5)
-pi.set_servo_pulsewidth(ESC, 0)
-time.sleep(2)
-print ("Arming ESC now...")
-pi.set_servo_pulsewidth(ESC, min_value)
-time.sleep(1)
-print ("Calibration Complete")
+inp = input("What is a the password?") 
+if inp == "idk":
+    pi.set_servo_pulsewidth(ESC, 0)
+    print("Calibration start")
+    pi.set_servo_pulsewidth(ESC, max_value)
+    pi.set_servo_pulsewidth(ESC, min_value)
+    time.sleep(7)
+    print ("Calibrating....")
+    time.sleep (5)
+    pi.set_servo_pulsewidth(ESC, 0)
+    time.sleep(2)
+    print ("Arming ESC now...")
+    pi.set_servo_pulsewidth(ESC, min_value)
+    time.sleep(1)
+    print ("Calibration Complete")
 
