16be2617596b58011534be598b9c5f8cd5834037
in-sol-ence <sol.oberoi.m@gmail.com>
2024-09-16 21:03:12 -0500

    Added Motor Control-our

--- src/CalibCode/MotorControl-our.py
+++ src/CalibCode/MotorControl-our.py
@@ -1,28 +1,50 @@
-import os
-import time
-os.system ("sudo pigpiod")
-time.sleep(1)
-import pigpio
+import os     #importing os library so as to communicate with the system
+import time   #importing time library to make Rpi wait because its too impatient 
+os.system ("sudo pigpiod") #Launching GPIO library
+time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+import pigpio #importing GPIO library
+
+ESC=15  #Connect the ESC in this GPIO pin 
 
 pi = pigpio.pi()
-ESC = 15
 pi.set_servo_pulsewidth(ESC, 0) 
 
-max_value = 1500 
-min_value = 500
-inp = input("What is a the password?") 
-if inp == "idk":
+max_value = 2000 #change this if your ESC's max value is different or leave it be
+min_value = 700  #change this if your ESC's min value is different or leave it be
+
+def calibrate():   #This is the auto calibration procedure of a normal ESC
     pi.set_servo_pulsewidth(ESC, 0)
-    print("Calibration start")
-    pi.set_servo_pulsewidth(ESC, max_value)
-    pi.set_servo_pulsewidth(ESC, min_value)
-    time.sleep(7)
-    print ("Calibrating....")
-    time.sleep (5)
-    pi.set_servo_pulsewidth(ESC, 0)
-    time.sleep(2)
-    print ("Arming ESC now...")
-    pi.set_servo_pulsewidth(ESC, min_value)
+    print("Disconnect the battery and press Enter")
+    inp = input()
+    if inp == '':
+        pi.set_servo_pulsewidth(ESC, max_value)
+        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
+        inp = input()
+        if inp == '':
+            pi.set_servo_pulsewidth(ESC, min_value)
+            print ("Wierd eh! Special tone")
+            time.sleep(7)
+            print ("Wait for it ....")
+            time.sleep (5)
+            print ("Im working on it, DONT WORRY JUST WAIT.....")
+            pi.set_servo_pulsewidth(ESC, 0)
+            time.sleep(2)
+            print ("Arming ESC now...")
+            pi.set_servo_pulsewidth(ESC, min_value)
+            time.sleep(1)
+            print ("See.... uhhhhh")
+
+def control(): 
+    print ("I'm Starting the motor, I hope its calibrated and armed, if not restart by giving 'x'")
     time.sleep(1)
-    print ("Calibration Complete")
+    speed = 700    # change your speed if you want to.... it should be between 700 - 2000
+    print ("Enter a PWM value from 700 to 2000. The car will go ahead for 1 second at these values. Measure how much they move and figure out the PWM to speed relationship")
+    while True:
+        pi.set_servo_pulsewidth(ESC, speed)
+        time.sleep(1)
+        pi.set_servo_pulsewidth(ESC, min)
+        speed = input("What PWM value do you want (-1 to break)")
+        speed = int(speed)
+        if (speed == -1):
+            break
 
