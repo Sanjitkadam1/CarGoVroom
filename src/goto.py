a62c9c0ca690a9f4827cb7dabd8e18f749bb958a
in-sol-ence <sol.oberoi.m@gmail.com>
2024-09-28 12:29:56 -0500

    added a stop function

--- src/goto.py
+++ src/goto.py
@@ -26,18 +26,44 @@ def checkPos(A, B):
     checky =  np.abs(A[1] - B[1]) <= tolerance
     return checkx and checky
 
+def stop():
+    esc = 18
+    pi.set_servo_pulsewidth(esc, 1500)
+    t.sleep(0.01)
+    pi.set_servo_pulsewidth(esc, 1300)
+    t.sleep(0.01)
+    pi.set_servo_pulsewidth(esc, 1500)
+    t.sleep(0.01)
+    pi.set_servo_pulsewidth(esc, 1300)
+    t.sleep(0.01)
+    pi.set_servo_pulsewidth(esc, 1500)
+
 #A and B are positions
 def goto(A, B):
-  angL = math.atan((B[1] - A[1])/(B[0] - A[0])) #gets the angle of the line
-  ang = getAngle() #gets the cars angle
-  Bservo(angL - ang) #changes the wheel angle to the difference between angL and ang
-  while not checkPos(postion(), B): #while we arent at B yet, 
-    pi.set_servo_pulsewidth(esc, 1570) #motor starts moving car
-    ang = getAngle() #gets cars angle
+    esc = 18
+    angL = math.atan((B[1] - A[1])/(B[0] - A[0])) #gets the angle of the line
+    ang = getAngle() #gets the cars angle
     Bservo(angL - ang) #changes the wheel angle to the difference between angL and ang
-  stop()
-  if(position() != B):
-    goto(position(),B) #if we arent at B yet it will recursively call the function again
-  else:
-    return "reached", B #if not then we end the function
-plt.show()
+    while not checkPos(postion(), B): #while we arent at B yet, 
+        pi.set_servo_pulsewidth(esc, 1570) #motor starts moving car
+        ang = getAngle() #gets cars angle
+        Bservo(angL - ang) #changes the wheel angle to the difference between angL and ang
+    stop()
+    if(position() != B):
+        goto(position(),B) #if we arent at B yet it will recursively call the function again
+    else:
+        return "reached", B #if not then we end the function
+    
+pi = pigpio.pi()
+esc = 18
+pi.set_servo_pulsewidth(esc, 0)
+print("say yes")
+inp = input()
+if inp.equals("no"):
+    exit()
+else:
+    pi.set_servo_pulsewidth(esc, 1500)
+    pi.set_servo_pulsewidth(esc, 1600)
+    t.sleep(1)
+
+stop()
--- src/healthCheck.py
+++ src/healthCheck.py
@@ -13,36 +13,6 @@ print("Imported all nessesary packages")
 
 #-------------------------Init Code-------------------------#
 
-# Depth init
-print("Echolocation Calibrating...")
-# if num == 0: #Front
-# 		TRIG = 17 #GPIO: 17, Pin 11
-# 		ECHO = 27 #GPIO: 27, Pin 13
-# 	if num == 1: #Left 
-# 		TRIG = 22 #GPIO: 22, Pin 15
-# 		ECHO = 23 #GPIO: 23, Pin 16
-# 	if num == 2: #Right
-# 		TRIG = 24 #GPIO: 24, Pin 18
-# 		ECHO = 25 #GPIO: 25, Pin 22
-TRIG1 = 17  # Front
-TRIG2 = 22  # Left
-TRIG3 = 24  # Right
-ECHO1 = 27  # Front
-ECHO2 = 23  # Left
-ECHO3 = 25  # Right
-# set values in the depth function
-PIN.setmode(PIN.BCM)
-PIN.setup(TRIG1, PIN.OUT)
-PIN.output(TRIG1, PIN.LOW)
-PIN.setup(ECHO1, PIN.IN)
-PIN.setup(TRIG2, PIN.OUT)
-PIN.output(TRIG2, PIN.LOW)
-PIN.setup(ECHO2, PIN.IN)
-PIN.setup(TRIG3, PIN.OUT)
-PIN.output(TRIG3, PIN.LOW)
-PIN.setup(ECHO3, PIN.IN)
-print("Echolocation Calibration Complete")
-
 #-------------------------Functions-------------------------#
 def depth(num):
 	#This code is for the Echo Sensors
