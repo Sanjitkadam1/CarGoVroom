6e0b365b333289c3e07292b6df9c78920b954e25
Sanjit Kadam <164111429+Sanjitkadam1@users.noreply.github.com>
2024-10-01 00:41:46 -0500

    Updated goto()

--- src/goto.py
+++ src/goto.py
@@ -39,7 +39,7 @@ print("Servo Calibrating...")
 servo = 14 #GPIO: 14, Pin: 8
 print("Servo Calibration Complete") 
 
-Camera init
+# Camera init
 print("Camera Calibrating...")
 picam = Picamera2()
 config = picam.create_still_configuration()
@@ -280,74 +280,76 @@ ax.plot(innerbox, label = "innerbox", color = 'black')
 outerbox = ([0,3000,3000,0,0],[0,0,3000,3000,0])
 ax.plot(outerbox, label = "outerbox", color = 'black')
 
-def detectObjs(track, turn):
-	picam.capture_file("test.jpeg")
-	img = cv.imread("test.jpeg")
-	height, width, channels = img.shape
-	hsvimg = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+def detectObjs():
+    picam.capture_file("test.jpeg")
+    img = cv.imread("test.jpeg")
+    height, width, channels = img.shape
+    hsvimg = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    side = ""
 
-	#Finding green 
-	greenLow = np.array([51, 100, 100])
-	greenHigh = np.array([61, 255, 255])
-	greenMask = cv.inRange(hsvimg, greenLow, greenHigh)
+    #Finding green 
+    greenLow = np.array([51, 100, 100])
+    greenHigh = np.array([61, 255, 255])
+    greenMask = cv.inRange(hsvimg, greenLow, greenHigh)
 
-	greenContours, greenHierarchy = cv.findContours(greenMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-	side = None
-	for contour in greenContours:
-		MAX = -1
-		x, y, Cwidth, Cheight = cv.boudingRect(contour)
-		if Cheight > height/10 and (Cwidth*Cheight) > MAX and Cwidth > width/10:
-			MAX = Cheight*Cwidth
-			if (MAX > minArea):
-				if (x > width/2 and (x-Cwidth) > width/2):
-					side = "left"
-				elif x < width/2: 
-					side = "right"
+    greenContours, greenHierarchy = cv.findContours(greenMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    side = None
+    for contour in greenContours:
+        MAX = -1
+        x, y, Cwidth, Cheight = cv.boudingRect(contour)
+        if Cheight > height/10 and (Cwidth*Cheight) > MAX and Cwidth > width/10:
+            MAX = Cheight*Cwidth
+            if (MAX > minArea):
+                if (x > width/2 and (x-Cwidth) > width/2):
+                    side = "left"
+                elif x < width/2: 
+                    side = "right"
 
-	if (side == "left"):
-		objGreen = object.obj(turn, num, "green")
-		track.add(objGreen)
-	elif (side == "right"):
-		objGreen = object.obj(turn, num+1, "green")
-		track.add(objGreen)
+	# if (side == "left"):
+	# 	objGreen = object.obj(turn, num, "green")
+	# 	track.add(objGreen)
+	# elif (side == "right"):
+	# 	objGreen = object.obj(turn, num+1, "green")
+	# 	track.add(objGreen)
 
 	#Finding red
-	lower_red1 = np.array([0, 100, 100])
-	upper_red1 = np.array([10, 255, 255])
-	lower_red2 = np.array([170, 100, 100])
-	upper_red2 = np.array([180, 255, 255])
+    lower_red1 = np.array([0, 100, 100])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([170, 100, 100])
+    upper_red2 = np.array([180, 255, 255])
 
-	redmask1 = cv.inRange(img, lower_red1, upper_red1)
-	redmask2 = cv.inRange(img, lower_red2, upper_red2)
-	redMask = cv.bitwise_or(redmask1, redmask2)
+    redmask1 = cv.inRange(img, lower_red1, upper_red1)
+    redmask2 = cv.inRange(img, lower_red2, upper_red2)
+    redMask = cv.bitwise_or(redmask1, redmask2)
 
-	redContours, redHierarchy = cv.findContours(redMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-	side = None
-	for contour in redContours:
-		MAX = -1
-		x, y, Cwidth, Cheight = cv.boudingRect(contour)
-		if Cheight > height/10 and (Cwidth*Cheight) > MAX and Cwidth > width/10:
-			MAX = Cheight*Cwidth
-			minArea = 0 # !!!! SET THIS V. IMP
-			if (MAX > minArea):
-				if (x > width/2 and (x-Cwidth) > width/2):
-					side = "left"
-				elif x < width/2: 
-					side = "right"
+    redContours, redHierarchy = cv.findContours(redMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    side = None
+    for contour in redContours:
+        MAX = -1
+        x, y, Cwidth, Cheight = cv.boudingRect(contour)
+        if Cheight > height/10 and (Cwidth*Cheight) > MAX and Cwidth > width/10:
+            MAX = Cheight*Cwidth
+            minArea = 0 # !!!! SET THIS V. IMP
+            if (MAX > minArea):
+                if (x > width/2 and (x-Cwidth) > width/2):
+                    side = "left"
+                elif x < width/2: 
+                    side = "right"
 	
-	if (objGreen.turn == "left" and not side == None):
-		print("ERROR DUPLICATE OBJECT DETECTED OR BINARY ERROR")
+	# if (objGreen.turn == "left" and not side == None):
+	# 	print("ERROR DUPLICATE OBJECT DETECTED OR BINARY ERROR")
 
-	if (side == "left"):
-		objRed = object.obj.__init__(turn, num, "red")
-		track.add(objRed)
-	elif (side == "right"):
-		if (objGreen.turn == "right"):
-			print("ERROR DUPLICATE OBJECT DETECTED")
-		objRed = object.obj.__init__(turn, num+1, "red")
-		track.add(objRed)
+	# if (side == "left"):
+	# 	objRed = object.obj.__init__(turn, num, "red")
+	# 	track.add(objRed)
+	# elif (side == "right"):
+	# 	if (objGreen.turn == "right"):
+	# 		print("ERROR DUPLICATE OBJECT DETECTED")
+	# 	objRed = object.obj.__init__(turn, num+1, "red")
+	# 	track.add(objRed)
 
-	return turn, num
+	# return turn, num
+    return side
 
 def reedsShep():
     # Run the bash script and capture the output
